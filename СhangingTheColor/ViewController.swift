c825d1414235ac084a25bfb7316ab9369924de98
Klim Sokolov <8klimsokolov@gmail.com>
2023-03-26 22:36:52 +0200

    SetUP constraints

--- СhangingTheColor/ViewController.swift
+++ СhangingTheColor/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var changingColorView: UIView!
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    @IBOutlet var labelForRedSlider: UILabel!
+    @IBOutlet var labelForGreenSlider: UILabel!
+    @IBOutlet var labelForBlueSlider: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        changingColorView.layer.cornerRadius = 20
+        slidersAction()
     }
-
-
+    
+    @IBAction func slidersAction() {
+        labelForRedSlider.text = redSlider.value.formatted(.number.precision(.fractionLength(2)))
+        labelForGreenSlider.text = greenSlider.value.formatted(.number.precision(.fractionLength(2)))
+        labelForBlueSlider.text = blueSlider.value.formatted(.number.precision(.fractionLength(2)))
+        
+        changingColorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
+    
 }
 
